551fbb36c9d36637d07db225cd7bc7b832dda6bf
Jlassi Achraf <128960056+LazySeaSeal@users.noreply.github.com>
2025-04-24 23:26:13 +0100

    Time: 11 ms (92.26%), Space: 42.8 MB (40.45%) - LeetHub

---
+++ 2856-count-complete-subarrays-in-an-array/2856-count-complete-subarrays-in-an-array.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int countCompleteSubarrays(vector<int> &v)
+{
+    int res = 0;
+    int d = 0;
+    unordered_set<int> us;
+    unordered_multiset<int> ms;
+    for (int &i : v)
+        us.insert(i);
+    int n = us.size();
+    int curr = 0;
+    for (int f = 0; f < v.size(); f++)
+    {
+        if (!ms.count(v[f])) //not there
+            curr++;
+        ms.insert(v[f]);
+        while (curr == n)
+        {
+            res += (v.size() - f) ;
+            if (ms.count(v[d]) == 1)
+                curr--;
+            ms.erase(ms.find(v[d++]));
+        }
+    }
+    return res;
+}
+};
