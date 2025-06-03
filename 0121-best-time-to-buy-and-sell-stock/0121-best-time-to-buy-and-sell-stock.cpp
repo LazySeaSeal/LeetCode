a23886383ef342ddde482248efca6595ac62b846
Jlassi Achraf <128960056+LazySeaSeal@users.noreply.github.com>
2025-06-03 12:26:32 +0200

    Time: 3 ms (29.17%), Space: 97.5 MB (26.46%) - LeetHub

---
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& v) {
+        int d=0,f,res=0;
+   while(d<v.size()-1)
+   {
+        for(f=d+1;f<v.size();f++)
+        {
+            int diff=v[f]-v[d];
+            if (diff>=0)
+                res=max(res,diff);
+            else
+                break;
+        }
+        d=f;
+   }
+   return(res);
+}
+};
