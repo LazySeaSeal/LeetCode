4d46ea5a9d2107170aa3c146da99c3b6d029ff09
Jlassi Achraf <128960056+LazySeaSeal@users.noreply.github.com>
2026-07-12 00:51:48 +0100

    Time: 0 ms (100%), Space: 15 MB (19.18%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -2,12 +2,16 @@ class Solution {
 public:
    vector<int> twoSum(vector<int>& v, int a) {
     unordered_map<int,int> umap;
+    int need=0;
         for(int i=0;i<v.size();i++)
-            umap[v[i]]=i;
-    for(int i=0;i<v.size();i++)
-        if (umap[a-v[i]] && umap[a-v[i]]!=i)
-            return {i,umap[a-v[i]]};
+            {
+                need=a-v[i];
+                if((umap.find(need) != umap.end()) && umap[need]!=i)
+                    return {i,umap[need]};
+                umap[v[i]]=i;
 
+            }
+ 
     return {};
 
 }
